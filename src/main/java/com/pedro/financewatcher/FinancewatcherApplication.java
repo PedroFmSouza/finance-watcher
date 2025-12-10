b0752be8f98dd00fc6b14a6627aec469b1b00550
Pedro Ferreira <pepeptu@gmail.com>
2025-12-10 08:02:15 -0300

    Projeto inicial Spring Boot

---
+++ src/main/java/com/pedro/financewatcher/FinancewatcherApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.financewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancewatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancewatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/financewatcher/FinancewatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.financewatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancewatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
